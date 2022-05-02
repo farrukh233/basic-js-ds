ef07e085172d5df70e8f707155bb5b2141747a72
farrukh <farrukhx50@gmail.com>
2022-05-02 23:27:06 +0500

    add removeKFromList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l == null) {
+    return l;
+  }
+  while (l.value == k) {
+    l = l.next;
+  }
+  let thisNode = l;
+  let nextNode = thisNode.next;
+  while (nextNode != null) {
+    if (nextNode.value == k) {
+      thisNode.next = nextNode.next;
+      if (thisNode.next == null) break;
+    }
+    thisNode = thisNode.next;
+    nextNode = thisNode.next;
+  }
+  return l;
 }
-
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };

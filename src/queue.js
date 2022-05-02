1741bbab17a3580953c5fe95eb802167edba260b
farrukh <farrukhx50@gmail.com>
2022-05-02 20:37:45 +0500

    add Queue task

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+function Queue() {
+  this.dataStore = [];
+  this.enqueue = enqueue;
+  this.dequeue = dequeue;
+}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+function enqueue(element) {
+  this.dataStore.push(element);
+}
+function dequeue() {
+  return this.dataStore.shift();
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
